a1c7d0bc7ee3f812fbfe1b9c36702db77d98db5b
GPSLAB776 <140515359+GPSLAB776@users.noreply.github.com>
2023-09-18 12:20:46 +0530

    fix drug null issue

--- app/src/main/java/com/devkraft/karmahealth/Adapter/AfternoonMedicenAdapter.java
+++ app/src/main/java/com/devkraft/karmahealth/Adapter/AfternoonMedicenAdapter.java
@@ -96,6 +96,9 @@ public class AfternoonMedicenAdapter extends RecyclerView.Adapter<AfternoonMedic
 
                     menu.findItem(R.id.menu2).setVisible(false);
                 }
+                if(mProductList.get(position).url == null){
+                    menu.findItem(R.id.menu1).setVisible(false);
+                }else
                 if(mProductList.get(position).url.length() >0){
 
                     menu.findItem(R.id.menu1).setVisible(true);
